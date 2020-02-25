5b753df4ea28e1fbf9d6699e3c6b3562872306a9
Byron Wang <stormlandon@outlook.com>
2020-02-25 22:13:47 +0800

    update conf and endpoint

--- src/utils/Endpoints.ts
+++ src/utils/Endpoints.ts
@@ -10,6 +10,7 @@ if (window._env_ && window._env_.API_URL) {
   endpoint = window._env_.API_URL;
 }
 
-export const VIDEO = `${endpoint}/v1/video`;
-export const SEARCH = `${endpoint}/v1/search`
-export const QUERY_STATUS = `${endpoint}/v1/status`
+export const VIDEO = `/v1/video`;
+export const SEARCH = `/v1/search`
+export const QUERY_STATUS = `/v1/status`
+
