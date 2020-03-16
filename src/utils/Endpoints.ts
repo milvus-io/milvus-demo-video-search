e2341ba2333721d8d8a556edcfd4fc97daf6e000
AzAz <az15921869329@gmail.com>
2020-03-16 17:31:06 +0800

    stage

--- src/utils/Endpoints.ts
+++ src/utils/Endpoints.ts
@@ -9,10 +9,10 @@ if (window._env_ && window._env_.API_URL) {
   endpoint = window._env_.API_URL;
 }
 
-export const VIDEO = `/v1/video`;
-export const SEARCH = `/v1/search`
-export const QUERY_STATUS = `/v1/status`
-// export const VIDEO = `${endpoint}/v1/video`;
-// export const SEARCH = `${endpoint}/v1/search`
-// export const QUERY_STATUS = `${endpoint}/v1/status`
+// export const VIDEO = `/v1/video`;
+// export const SEARCH = `/v1/search`
+// export const QUERY_STATUS = `/v1/status`
+export const VIDEO = `${endpoint}/v1/video`;
+export const SEARCH = `${endpoint}/v1/search`
+export const QUERY_STATUS = `${endpoint}/v1/status`
 
